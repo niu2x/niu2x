9ef4f1c7de7a1ac3e48e8c12bfbc2175abe2873f
niu2x <niu2x@icloud.com>
2021-08-17 00:48:26 +0800

    move

--- src/niu2x/string_utils.cpp
+++ src/niu2x/string_utils.cpp
@@ -1,5 +1,13 @@
 #include <niu2x/string_utils.h>
 
+#include <string.h>
+
+#include <sstream>
+
+#if !defined(_WIN32) || !defined(_WIN64)
+    #define strncpy_s strncpy
+#endif
+
 namespace nx {
 
 std::vector<std::string> string_utils::split(
@@ -21,6 +29,35 @@ std::vector<std::string> string_utils::split(
     }
 
     return results;
+}
+
+std::string string_utils::string(const struct sockaddr* sa)
+{
+    std::stringstream ss;
+    char address[INET_ADDRSTRLEN];
+    uint16_t port = 0;
+    switch (sa->sa_family) {
+        case AF_INET:
+            inet_ntop(AF_INET, &(((struct sockaddr_in*)sa)->sin_addr), address,
+                INET_ADDRSTRLEN);
+            port = ((struct sockaddr_in*)sa)->sin_port;
+            break;
+
+        case AF_INET6:
+            inet_ntop(AF_INET6, &(((struct sockaddr_in6*)sa)->sin6_addr),
+                address, INET_ADDRSTRLEN);
+            port = ((struct sockaddr_in6*)sa)->sin6_port;
+            break;
+
+        default:
+            strncpy_s(address, "Unknown AF", INET_ADDRSTRLEN);
+            return NULL;
+    }
+    ss << address;
+    ss << " ";
+    ss << ntohs(port);
+
+    return ss.str();
 }
 
 } // namespace nx
--- src/niu2x/string_utils.h
+++ src/niu2x/string_utils.h
@@ -1,6 +1,17 @@
 #ifndef NX_STRING_UTILS_H
 #define NX_STRING_UTILS_H
 
+#if defined(__linux__)
+    #include <sys/socket.h>
+    #include <arpa/inet.h>
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+    #include <Winsock2.h>
+    #include <winsock.h>
+    #include <ws2tcpip.h>
+#endif
+
 #include <string>
 #include <vector>
 #include <string_view>
@@ -12,6 +23,8 @@ namespace nx {
 struct API string_utils {
     static std::vector<std::string> split(
         const std::string_view& sz, uint8_t delimiter);
+
+    static std::string string(const struct sockaddr* sa);
 };
 
 } // namespace nx
