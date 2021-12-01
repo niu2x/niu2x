f7f55bc4fe041e7e1a7ef8757983d969637f6c7b
niu2x <niu2x@icloud.com>
2021-12-02 00:03:18 +0800

    remove stb font

--- src/niu2x/gfx/object.cpp
+++ src/niu2x/gfx/object.cpp
@@ -10,12 +10,6 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb/stb_image.h"
 
-#define STB_RECT_PACK_IMPLEMENTATION
-#include "stb/stb_rect_pack.h"
-
-#define STB_TRUETYPE_IMPLEMENTATION
-#include "stb/stb_truetype.h"
-
 #include <niu2x/fs.h>
 
 #include "niu2x/utils.h"
