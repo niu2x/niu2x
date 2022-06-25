c51d462103e30391e45ec010db3fcfabb6966379
niu2x <niu2x@icloud.com>
2022-06-25 23:30:32 +0800

    add mu-wan-qing picture

---
+++ test/mu-wan-qing.h
@@ -0,0 +1,15 @@
+#ifndef muwanqing__H
+#define muwanqing__H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern unsigned char muwanqing[];
+extern unsigned long muwanqing_length;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
